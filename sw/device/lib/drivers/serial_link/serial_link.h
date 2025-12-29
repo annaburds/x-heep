1c793b06b5ab4c6b45a09801955f6c2671d205b8
Burdina Anna <burdina@eslpc11.intranet.epfl.ch>
2025-12-29 15:08:40 +0100

    updated headers fro sl drivers

--- sw/device/lib/drivers/serial_link/serial_link.h
+++ sw/device/lib/drivers/serial_link/serial_link.h
@@ -1,3 +1,7 @@
+// Copyright 2025 EPFL
+// Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+
 
 #include <stdio.h>
 #include <stdint.h>
