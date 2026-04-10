0c438e76da9192bceb9a55a6bc2173f18caa0af5
Anna Burdina <anna.burdina@epfl.ch>
2026-04-10 10:52:41 +0200

    format python

--- hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
+++ hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
@@ -12,26 +12,26 @@ module xilinx_core_v_mini_mcu_wrapper
 ) (
 
 `ifdef FPGA_ZCU104
-    inout logic clk_300mhz_n,
-    inout logic clk_300mhz_p,
+    inout  logic clk_300mhz_n,
+    inout  logic clk_300mhz_p,
 `elsif FPGA_ZCU102
-    inout logic clk_125mhz_n,
-    inout logic clk_125mhz_p,
+    inout  logic clk_125mhz_n,
+    inout  logic clk_125mhz_p,
 `elsif FPGA_AUP_ZU3
-    inout logic clk_100mhz_n,
-    inout logic clk_100mhz_p,
+    inout  logic clk_100mhz_n,
+    inout  logic clk_100mhz_p,
 `elsif FPGA_GENESYS2
-    inout logic clk_200mhz_n,
-    inout logic clk_200mhz_p,
+    inout  logic clk_200mhz_n,
+    inout  logic clk_200mhz_p,
 `elsif FPGA_NEXYS
-    inout logic clk_i,
+    inout  logic clk_i,
 `else
-    inout logic clk_i,
+    inout  logic clk_i,
     // Serial Link DDR clock ports for PYNQ Z2 board
-    input  wire ddr_rcv_clk_i,
-    output wire ddr_snd_clk_o,
+    input  wire  ddr_rcv_clk_i,
+    output wire  ddr_snd_clk_o,
 `endif
-    inout logic rst_i,
+    inout  logic rst_i,
 
     output logic rst_led_o,
     output logic clk_led_o,
@@ -109,8 +109,8 @@ module xilinx_core_v_mini_mcu_wrapper
 `endif
 
 `ifdef NO_DDR_CLK_PORTS
-    wire ddr_rcv_clk_i;
-    wire ddr_snd_clk_o;
+  wire ddr_rcv_clk_i;
+  wire ddr_snd_clk_o;
 `endif
 
   // reset LED for debugging
